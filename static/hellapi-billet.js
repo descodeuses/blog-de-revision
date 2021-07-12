36bd47e3a3a39aa3ebdc32858888e87b1e17403e
Yannick Francois <yaf@elsif.fr>
2021-07-12 20:25:39 +0200

    Utilise l'API Fetch de JavaSript

--- static/hellapi-billet.js
+++ static/hellapi-billet.js
@@ -7,16 +7,16 @@ http://monserveur/billet/<id>.json GET
 let splittedLocation = window.location.toString().split("/")
 let id = splittedLocation[splittedLocation.length - 1]
 
-httpRequest = new XMLHttpRequest();
-httpRequest.open('GET', '/api/billet/' + id);
-httpRequest.send();
-httpRequest.onreadystatechange = function() {
-  if (httpRequest.readyState === XMLHttpRequest.DONE) {
-    // instructions de traitement de la réponse
-    let billet = JSON.parse(httpRequest.responseText)
-
+if (window.fetch) {
+  // exécuter ma requête fetch ici
+  console.log("mon navigateur réponds à fetch")
+  fetch('/api/billet/' + id)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(billet) {
     let section = document.createElement("section")
-
+  
     let title = document.createElement("h1")
     let textTitle = document.createTextNode(billet.title)
     title.appendChild(textTitle)
@@ -28,6 +28,32 @@ httpRequest.onreadystatechange = function() {
     section.appendChild(texte)
 
     document.getElementsByTagName("body")[0].appendChild(section)
+  })
+} else {
+  // Si le navigateur ne support pas l'API Fetch de JavaScript
+  httpRequest = new XMLHttpRequest();
+  httpRequest.open('GET', '/api/billet/' + id);
+  httpRequest.send();
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      // instructions de traitement de la réponse
+      let billet = JSON.parse(httpRequest.responseText)
+  
+      let section = document.createElement("section")
+  
+      let title = document.createElement("h1")
+      let textTitle = document.createTextNode(billet.title)
+      title.appendChild(textTitle)
+      section.appendChild(title)
+  
+      let texte = document.createElement("p")
+      let texteContent = document.createTextNode(billet.text)
+      texte.appendChild(texteContent)
+      section.appendChild(texte)
+  
+      document.getElementsByTagName("body")[0].appendChild(section)
+  
+    }
+  };
+}
 
-  }
-};
--- static/hellapi.js
+++ static/hellapi.js
@@ -7,15 +7,14 @@ pour obtenir la liste des billets de blog
 http://monserveur/billets GET
 */
 
-// https://developer.mozilla.org/fr/docs/Web/Guide/AJAX/Getting_Started
-httpRequest = new XMLHttpRequest();
-httpRequest.open('GET', '/api/billets');
-httpRequest.send();
-httpRequest.onreadystatechange = function() {
-  if (httpRequest.readyState === XMLHttpRequest.DONE) {
-    // instructions de traitement de la réponse
-    let billets = JSON.parse(httpRequest.responseText)
-
+if (window.fetch) {
+  // exécuter ma requête fetch ici
+  console.log("mon navigateur réponds à fetch")
+  fetch('/api/billets')
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(billets) {
     let ol = document.createElement("ol")
     billets.forEach(function(billet) {
       let li = document.createElement("li")
@@ -27,5 +26,30 @@ httpRequest.onreadystatechange = function() {
       ol.appendChild(li)
     })
     document.getElementById("list-billets").appendChild(ol)
-  }
-};
+  })
+} else {
+  // Si le navigateur ne support pas l'API Fetch de JavaScript
+  // https://developer.mozilla.org/fr/docs/Web/Guide/AJAX/Getting_Started
+  httpRequest = new XMLHttpRequest();
+  httpRequest.open('GET', '/api/billets');
+  httpRequest.send();
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      // instructions de traitement de la réponse
+      let billets = JSON.parse(httpRequest.responseText)
+
+      let ol = document.createElement("ol")
+      billets.forEach(function(billet) {
+        let li = document.createElement("li")
+        let link = document.createElement("a")
+        link.setAttribute("href", "/billet/" + billet.id)
+        let content = document.createTextNode(billet.title)
+        link.appendChild(content)
+        li.appendChild(link)
+        ol.appendChild(li)
+      })
+      document.getElementById("list-billets").appendChild(ol)
+    }
+  };
+}
+
